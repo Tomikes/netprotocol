0c177c1025b0e981f72d042b600dcafa74f86251
Tomikes <mike_shang@outlook.com>
2016-08-15 08:27:45 +0800

    Initial Commit

---
+++ netprotocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netprotocol
+//
+//  Created by mike on 8/15/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ netprotocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netprotocol
+//
+//  Created by mike on 8/15/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
